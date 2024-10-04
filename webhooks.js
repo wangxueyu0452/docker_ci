7cc0c8782b3d0e23a720f682d9f030c12f156783
wangxueyu0452 <350047663@qq.com>
2024-10-04 17:55:38 +0800

    'fix:testwebhook'

--- webhooks.js
+++ webhooks.js
@@ -1,57 +1,25 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks', secret: 'myHashSecret' })
-// 上面的 secret 保持和 GitHub 后台设置的一致
+const http = require('http')
+const createHandler = require('github-webhook-handler')
+const handler = createHandler({
+    path: '/docker_deploy',
+    secret: 'mySecret',
 
-function run_cmd(cmd, args, callback) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
-    var resp = "";
+})
 
-    child.stdout.on('data', function (buffer) { resp += buffer.toString(); });
-    child.stdout.on('end', function () { callback(resp) });
-}
-// debug用
-// run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-
-http.createServer(function (req, res) {
-
-    handler(req, res, function (err) {
+http.createServer((req, res) => {
+    handler(req, res, err => {
         res.statusCode = 404
         res.end('no such location')
     })
-}).listen(7777,() =>{
-    console.log('WebHooks Listern at 7777');
+}).listen(7777, () => {
+    console.log('Webhook listen at 7777')
 })
 
-handler.on('error', function (err) {
-    console.error('Error:', err.message)
+handler.on('error', err => {
+    console.error('Error', err.message)
 })
 
-
-handler.on('*', function (event) {
-    console.log('Received *', event.payload.action);
-    //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
+// handler.on('*') 所有的行为
+handler.on('push', event => {
+    console.log('Revice push', event.payload )
 })
- 
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref);
-        // 分支判断
-        if(event.payload.ref === 'refs/heads/master'){
-            console.log('deploy master..')
-            run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-
-        }
-})
-
-
-// handler.on('issues', function (event) {
-//     console.log('Received an issue event for % action=%s: #%d %s',
-//         event.payload.repository.name,
-//         event.payload.action,
-//         event.payload.issue.number,
-//         event.payload.issue.title)
-// })
-
